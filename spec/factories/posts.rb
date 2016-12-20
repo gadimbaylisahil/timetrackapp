4ab765e876f8b2c7b9abfcb6c943cf6eea907981
Sahil <sahil.gadimbay@gmail.com>
2016-12-21 00:22:20 +0100

    implemented enum for status of posts for approval worklow

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do 
 	factory :post do
 		date Date.today
-		rationale "Rationale Example"
+		rationale "Rationale Example" 
 		user
 	end
 	factory :second_post, class: "Post" do
 		date Date.yesterday
-		rationale "Rationale Example 2"
+		rationale "Rationale Example 2"  
 		user
 	end
 end
