f0bfe1c4f423a59371be65edf199cf254b130d1f
Sahil <sahil.gadimbay@gmail.com>
2016-12-21 03:10:38 +0100

    showing users only their own posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,21 +6,34 @@ describe 'navigate' do
 		login_as(@user, :scope => :user)
 	end 
 	describe 'index' do
-		it 'can be reached successfully' do
+		before do 
 			visit posts_path
+		end
+
+		it 'can be reached successfully' do
 			expect(page.status_code).to eq(200)
 		end
 
 		it 'has a title of Posts' do
-			visit posts_path
 			expect(page).to have_content(/Posts/)
 		end
 
 		it 'has a list of posts' do
-			post1 = FactoryGirl.create(:post)
-			post2 = FactoryGirl.create(:second_post)
+			post1 = FactoryGirl.build_stubbed(:post)
+			post2 = FactoryGirl.build_stubbed(:second_post)
 			visit posts_path
-			expect(page).to have_content(/Rationale Example|Rationale Example 2/)
+			expect(page).to have_content(/Rationale|Example/)
+		end
+
+		it 'only lets post creators to see their own posts' do 
+			post1 = Post.create(date: Date.today, rationale: "some rationale", user_id: @user.id)
+			post1 = Post.create(date: Date.today, rationale: "some rationale", user_id: @user.id)
+
+			other_user = User.create(first_name: 'Test', last_name: "TestSurname", email: "some@some.com", password: "123456", password_confirmation: "123456")
+			post_from_other_user = Post.create(date: Date.today, rationale: "some rationale shouldnt be seen", user_id: other_user.id)
+			
+			visit posts_path
+			expect(page).to_not have_content(/some rationale shouldnt be seen/)
 		end
 	end
 
@@ -28,12 +41,16 @@ describe 'navigate' do
 		it 'has a link from the homepage' do
 			visit root_path
 			click_link("new_post_from_nav") 
+			expect(page.status_code).to eq(200)
 		end
 	end
 
   describe 'delete' do
     it 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      # refactor later
+      @post.update(user_id: @user.id)
+
       visit posts_path
 
       click_link("delete_post_#{@post.id}_from_index")
