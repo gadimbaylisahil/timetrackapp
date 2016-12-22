c7150f62a12f363a2694e849045b8b29bd385338
Sahil <sahil.gadimbay@gmail.com>
2016-12-22 18:36:56 +0100

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-@user = User.create(email: "regular@regular.com",
-					password: "123456",
-					password_confirmation: "123456",
-					first_name: "Regular", 
-					last_name: "RegularSurname",
-					phone: "0034631703581" )
-puts "Regular user has been created"
 
 AdminUser.create(email: "admin@admin.com", 
 				 password: "123456", 
@@ -21,6 +14,14 @@ AdminUser.create(email: "admin@admin.com",
 				 phone: "0034631703581")
 
 puts "Admin user has been created"
+
+@user = User.create(email: "regular@regular.com",
+					password: "123456",
+					password_confirmation: "123456",
+					first_name: "Regular", 
+					last_name: "RegularSurname",
+					phone: "0034631703581" )
+puts "Regular user has been created"
 
 25.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale", user_id: @user.id, overtime_request: 2.5)
