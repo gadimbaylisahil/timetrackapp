56de5c74ae5e5a970d0265433c4587bea6017421
Sahil <sahil.gadimbay@gmail.com>
2016-12-23 00:33:20 +0100

    Rspec for Audit_Logs index controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :audit_logs
+  resources :audit_logs, except: [:new, :edit, :destroy]
   namespace :admin do
-    resources :users
-resources :posts
-resources :admin_users
-
-    root to: "users#index"
+  resources :users
+	resources :posts
+  resources :admin_users
+	root to: "users#index"
   end
 
   resources :posts
